a09b38ba4f091a9a9f7c155991616967ba1bfef6
jswp <3028467763@qq.com>
2023-03-19 14:38:39 +0800

    添加数据类型定义到 rtdef.h

---
+++ rtthread/3.0.3/include/libc/rtdef.h
@@ -0,0 +1,31 @@
+#ifndef __RT_DEF_H__
+#define __RT_DEF_H__
+
+
+// RT  基础类型重定义
+typedef signed char  rt_int8_t;
+typedef signed short rt_int16_t;
+typedef signed long  rt_int32_t;
+typedef unsigned char rt_uint8_t;
+typedef unsigned short rt_uint16_t;
+typedef unsigned long rt_uint32_t;
+typedef int rt_bool_t;
+
+/* 32bit CPU */
+typedef long rt_base_t; /* the base type of 32bit CPU */
+typedef unsigned long rt_ubase_t;
+typedef rt_base_t rt_err_t;
+typedef rt_uint32_t rt_time_t;
+typedef rt_uint32_t rt_tick_t;
+typedef rt_base_t rt_flag_t;
+typedef rt_ubase_t rt_size_t;
+typedef rt_ubase_t rt_dev_t;
+typedef rt_base_t rt_off_t;
+
+/*redefine the  bool type  */
+#define RT_TRUE 1
+#define RT_FALSE 0
+
+    
+
+#endif
